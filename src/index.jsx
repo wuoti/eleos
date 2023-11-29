2c70da93263de3705090baf4086a4312a4645298
Kalle Wuoti <kalle.wuoti@reaktor.com>
2023-11-29 16:19:39 +0100

    Initial commit

---
+++ src/index.jsx
@@ -0,0 +1,38 @@
+import api, { route } from "@forge/api"
+import ForgeUI, {
+  Fragment,
+  IssuePanel,
+  Text,
+  render,
+  useProductContext,
+  useState,
+} from "@forge/ui"
+
+const fetchCommentsForIssue = async (issueIdOrKey) => {
+  const res = await api
+    .asUser()
+    .requestJira(route`/rest/api/3/issue/${issueIdOrKey}/comment`)
+
+  const data = await res.json()
+  return data.comments
+}
+
+const App = () => {
+  const context = useProductContext()
+  const [comments] = useState(
+    async () => await fetchCommentsForIssue(context.platformContext.issueKey),
+  )
+  console.log(`Number of comments on this issue: ${comments.length}`)
+
+  return (
+    <Fragment>
+      <Text>Hello world!</Text>
+    </Fragment>
+  )
+}
+
+export const run = render(
+  <IssuePanel>
+    <App />
+  </IssuePanel>,
+)
